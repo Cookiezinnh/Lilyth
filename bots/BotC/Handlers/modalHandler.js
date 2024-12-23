d08fe42987a0d871ee89417f6e176a70ae13b498
Cookiezinnh <68929584+Cookiezinnh@users.noreply.github.com>
2024-12-22 21:21:04 -0300

    Delete bots/BotC directory

--- bots/BotC/Handlers/modalHandler.js
+++
@@ -1,9 +0,0 @@
-// Exemplo para gerenciar modals
-module.exports = (client) => {
-    client.on('interactionCreate', interaction => {
-        if (!interaction.isModalSubmit()) return;
-        const modal = client.modals.get(interaction.customId);
-        if (!modal) return;
-        modal.execute(interaction);
-    });
-};
